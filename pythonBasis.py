7d7f537be54596f66f9274aace4ad93d7b7d6ff2
Esther Weyinmi Koloh <kolohw@gmail.com>
2018-12-22 14:54:27 +0100

    Examples to explain the python case sensitivity to of variables names.

--- pythonBasis.py
+++ pythonBasis.py
@@ -3,3 +3,28 @@ print('Hello World')
 #1. Must start with a letter or underscore. Cannot start with a number
 #2. Consist of letters, numbers or understand.
 #3. Python variables are case sensitive.
+
+# Variable name with just letters in lowercase
+car = "Mercedes"
+print(car)
+
+# Variable name starting with underscore
+_car = "Audi"
+print(_car)
+
+# Variable name with letters, underscore and numbers
+Two_Wheeler121 = "Royal Enfield"
+print(Two_Wheeler121)
+
+# Variable name with just letters in upercase
+CAR = "Jaguar"
+print(CAR)
+
+# Variable name with first letter capitalized
+Car = "BMW"
+print(Car)
+
+"""Summarry of Python naming covention rule #3.
+Variable names are case sensitive, that is, Car,CAR,car are totally different variables
+"""
+
