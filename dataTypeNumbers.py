17496c0102ccc57708fcb3fb9c0f828a787bb0fd
Esther Weyinmi Koloh <kolohw@gmail.com>
2018-12-22 17:14:33 +0100

    added Number data type with examples

---
+++ dataTypeNumbers.py
@@ -0,0 +1,19 @@
+"""
+We have integers and floats
+"""
+a = 14
+print(a)
+
+b = 4
+print(b)
+
+print(a + b)        # print addition of a and b, 18.
+print(a - b)        # print subtraction of a and b, 10.
+print(a * b)        # print multiplication of a and b, 56.
+print(a / b)        # print division of a and b in the complete decimal format, -3.5.
+print(a // b)       # print division of a and b but ignores everything after the decimal point, -3.
+print(a % b)        # Only print the remainder value as a result of the division, -2.
+
+print(2+10*10+3)    #Result:105
+
+print(5 + 20 / 4 * 2 - 7) #Rsult:8.0
