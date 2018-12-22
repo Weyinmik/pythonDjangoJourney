e9658b851b217f4338c8a414fd49a7f907f8ff92
Esther Weyinmi Koloh <kolohw@gmail.com>
2018-12-22 14:41:00 +0100

    added python naming convention rules.

--- pythonBasis.py
+++ pythonBasis.py
@@ -1,1 +1,5 @@
 print('Hello World')
+#Python Naming Rules
+#1. Must start with a letter or underscore. Cannot start with a number
+#2. Consist of letters, numbers or understand.
+#3. Python variables are case sensitive.
