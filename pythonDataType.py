e80e152d4059eada1c9d8eeebe56c8d1e255c324
Esther Weyinmi Koloh <kolohw@gmail.com>
2018-12-22 15:12:34 +0100

    added things to note in 7 data types in python.

--- pythonDataType.py
+++ pythonDataType.py
@@ -3,5 +3,17 @@ THINGS TO NOTE:
 1. Every value in Python has a data type.
 2. Everything is an object in Python programming.
 3. Data types are classes. (Blue prints)
-4. Variables are object of classes.
+4. Variables are objects (any value stored in computer memory is an object) of classes.
+
+7 DATA TYPES IN PYTHON
+a. Numbers - whole numbers oof decimal numbers
+b. Strings - ordered sequence of characters and written in single or double quotes.
+c. List-written in square brackets []. Are ordered sequence of objects
+d. Dictionaries - Unordered key value pairs. written in curly brackets {}
+e. Tuples - ordered immutables sequence of objects. written in parenthesis ()
+f. Sets - unordered collection of unique objects ("a", "b")
+g. Logical values True of False
 """
+
+# Strings
+
