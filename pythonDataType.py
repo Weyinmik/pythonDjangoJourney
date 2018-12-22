3f3e4b135a065c6437a42f773c8bb1ef11ca1944
Esther Weyinmi Koloh <kolohw@gmail.com>
2018-12-22 15:00:21 +0100

    added things to note in Python Data Type.

---
+++ pythonDataType.py
@@ -0,0 +1,7 @@
+"""
+THINGS TO NOTE:
+1. Every value in Python has a data type.
+2. Everything is an object in Python programming.
+3. Data types are classes. (Blue prints)
+4. Variables are object of classes.
+"""
