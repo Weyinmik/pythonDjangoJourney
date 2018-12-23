40a5e04fa02af7b2b656869949e14f21149948ca
Esther Weyinmi Koloh <kolohw@gmail.com>
2018-12-23 14:38:10 +0100

    Added data type Boolean with examples

---
+++ dataTypeBoolean.py
@@ -0,0 +1,16 @@
+"""
+Logical values that convey a statement of true or false
+
+Booleans are very important in control flow and logic
+"""
+print(True)                 #Result: True
+
+print(true)                 #Result: true not defined
+
+print(False)                 #Result: False
+
+print(type(True))                 #Result: bool
+
+print(10 > 9)                 #Result: True
+
+print(7 > 9)                 #Result: False
