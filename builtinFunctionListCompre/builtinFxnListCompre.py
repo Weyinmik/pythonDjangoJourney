4cfdcc6691faf3964d0d68a46212c682dc2e6daf
Esther Weyinmi Koloh <kolohw@gmail.com>
2019-01-11 13:56:31 +0100

    added examples of range of numbers.

--- builtinFunctionListCompre/builtinFxnListCompre.py
+++ builtinFunctionListCompre/builtinFxnListCompre.py
@@ -11,3 +11,14 @@ The list comprehension always returns a result list
 """
 ## Mostly used Built-in functions in the Python
 #1. Range Function
+for num in range(10):
+    print(num)      # This generates sequence of numbers in the form of list (0-9)
+
+for num in range(3,11):
+    print(num)      # This generates sequence of numbers in the form of list (3-10)
+
+for num in range(3,11,2):
+    print(num)      # This generates sequence of numbers in the form of list (3,5,7,9)
+                    # The 2 is the difference between the numbers
+
+
